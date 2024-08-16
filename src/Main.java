4d9fb98d3f915937aae3bf8d61df36c310af7340
marcosdanilocastreghini <danilocastreghini@gmail.com>
2024-08-16 19:15:33 -0400

    Aula 2 iniciando uma calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,23 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static int max_value = 100;
-    private static String FUNCTION_NAME = "Main Function";
-
     // Exemplo de chamada de metodo nao estatico
     public static void main(String[] args) {
-        System.out.printf("Hello World");
-        //instanciando objetos do tipo MinhaCalculadora
-        var objetoCalculadora = new MinhaCalculadora();
-        MinhaCalculadora objetoCalculadora2 = new MinhaCalculadora();
+        System.out.println("Bem vindo a calculadora");
+        System.out.println("Qual tipo de calculo voce quer fazer?");
+        System.out.println("1. Somar");
+        System.out.println("2. Subtrair");
 
-        //Acessando metodo dentro dos objetos
-        var somatorio = objetoCalculadora.soma(2, 4);
-        int subtracao = objetoCalculadora2.subtrair(4, 5);
-        System.out.printf("O somatorio é: " + somatorio);
-        System.out.printf("A subtracao é: "+ subtracao);
+        //instanciando objetos do tipo MinhaCalculadora
+//        var objetoCalculadora = new MinhaCalculadora();
+//        MinhaCalculadora objetoCalculadora2 = new MinhaCalculadora();
+//
+//        //Acessando metodo dentro dos objetos
+//        var somatorio = objetoCalculadora.soma(2, 4);
+//        int subtracao = objetoCalculadora2.subtrair(4, 5);
+//
+//        System.out.println("O somatorio é: " + somatorio);
+//        System.out.println("A subtracao é: "+ subtracao);
     }
 
 
