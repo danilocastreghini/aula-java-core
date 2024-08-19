88a8e5b360ffe99abc990ab0a5feaa7f105efbb0
marcosdanilocastreghini <danilocastreghini@gmail.com>
2024-08-19 19:06:44 -0400

    Aula 3 Exemplo Carteira Motorista

--- src/ExemploCarteiraMotorista.java
+++ src/ExemploCarteiraMotorista.java
@@ -7,10 +7,15 @@ public class ExemploCarteiraMotorista {
      int idade = entrada.nextInt();
         System.out.println("Voce possui carteira de motorista? ");
         boolean possuiCarteira = entrada.nextBoolean();
+        System.out.println("Voce esta bebado? ");
+        boolean estaBebado = entrada.nextBoolean();
         //operacao logica
-
-
-
-
+        if (idade >= 18 && possuiCarteira && !estaBebado) {
+            System.out.println("Voce pode dirigir");
+        } else {
+                System.out.println("Voce nao pode dirigir");
+            }
+        entrada.close();
+        }
     }
-}
+
