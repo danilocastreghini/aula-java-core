f9ca933531c020ef275dee8dce6e8cf03f7929e6
marcosdanilocastreghini <danilocastreghini@gmail.com>
2024-08-19 18:54:26 -0400

    Aula 3 Exemplo Carteira Motorista

---
+++ src/ExemploCarteiraMotorista.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExemploCarteiraMotorista {
+    public static void main(String[] args){
+     Scanner entrada = new Scanner(System.in);
+     System.out.println("Qual a sua idade? ");
+     int idade = entrada.nextInt();
+        System.out.println("Voce possui carteira de motorista? ");
+        boolean possuiCarteira = entrada.nextBoolean();
+        //operacao logica
+
+
+
+
+    }
+}
